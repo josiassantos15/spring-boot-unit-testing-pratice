48a1bc46f60f8eed00205697aba5e2fea79605a9
Josiassantos1577 <josiassantos1577@gmail.com>
2024-06-24 16:33:25 -0300

    test: Desabilitando um teste em um ambiente específico com variável de ambiente

--- src/test/java/com/josias/spring_boot_unit_testing_practice/utilitarios/PessoaTest.java
+++ src/test/java/com/josias/spring_boot_unit_testing_practice/utilitarios/PessoaTest.java
@@ -1,8 +1,10 @@
 package com.josias.spring_boot_unit_testing_practice.utilitarios;
 
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +16,19 @@ class PessoaTest {
     @DisplayName("Asserções agrupadas")
     void assertAllPessoa() {
         Pessoa pessoa = new Pessoa("John", "Doe");
+        assertAll("Asserções de pessoa",
+                () -> assertEquals("John", pessoa.getNome()),
+                () -> assertEquals("Doe", pessoa.getSobrenome()),
+                () -> assertEquals("John Doe", pessoa.getNomeCompleto()));
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
+    @DisplayName("Asserções agrupadas desabilita teste em PROD")
+    void assertAllPessoa_disabledIfEnvironmentVariable() {
+        Pessoa pessoa = new Pessoa("John", "Doe");
+        Assumptions.assumeTrue("PROD".equals(System.getenv("PROD")),
+                () -> "Abortando teste: Não deve ser executado em PROD");
         assertAll("Asserções de pessoa",
                 () -> assertEquals("John", pessoa.getNome()),
                 () -> assertEquals("Doe", pessoa.getSobrenome()),
