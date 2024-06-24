1e71d03696438320160e42e1155b522d69ec2265
Josiassantos1577 <josiassantos1577@gmail.com>
2024-06-24 17:31:58 -0300

    refactor: Atualizando regra de negocio da class Saudacao e organizando testes da classe para o padrão Triple A

--- src/test/java/com/josias/spring_boot_unit_testing_practice/utilitarios/SaudacaoUtilTest.java
+++ src/test/java/com/josias/spring_boot_unit_testing_practice/utilitarios/SaudacaoUtilTest.java
@@ -2,6 +2,7 @@ package com.josias.spring_boot_unit_testing_practice.utilitarios;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,17 +10,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class SaudacaoUtilTest {
 
     @Test
-    @DisplayName("deve retornar saudacao de acordo com a hora informada")
+    @DisplayName("deve retornar saudacao Bom dia à partir de 5h")
     void saudar() {
-        String saudacao = SaudacaoUtil.saudar(10);
+        //Arrange
+        int horaValida = 5;
+
+        //Act
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+
+        //Assert
         assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    }
+
+    @Test
+    @DisplayName("deve retornar saudacao Boa Noite até 4h")
+    void saudar_boaNoite() {
+        int horaValida = 4;
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+        assertEquals("Boa noite", saudacao, "Saudação incorreta!");
     }
 
     @Test
     @DisplayName("deve retornar exception ao informar hora errada")
     void saudar_exception() {
+        int horaInvalida = -10;
+        Executable executable = () -> SaudacaoUtil.saudar(horaInvalida);
         var exception = assertThrows(IllegalArgumentException.class,
-                () -> SaudacaoUtil.saudar(-10),
+                executable,
                 "A exception não foi lançada corretamente");
         assertEquals("Hora inválida", exception.getMessage());
     }
